c05ff8f0644a478150ed1338ee469b01e811f8ec
Abhirami_AnilKumar <81986508+abhiramianilnair@users.noreply.github.com>
2023-01-17 11:33:51 +0530

    Add files via upload

---
+++ CybageOnlineMealorderingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cybage_online_mealorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybageOnlineMealorderingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybageOnlineMealorderingsystemApplication.class, args);
+	}
+
+}
